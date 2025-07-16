603ad2782af7d612f14a2edc2be784733759f1ff
cccccccccccccccccnrd <hello@conradweise.com>
2025-07-16 14:04:02 +0200

    u

--- public/v.sh
+++ public/v.sh
@@ -10,6 +10,8 @@ while true; do
     seconds=$(echo "$response" | grep -o '"seconds":[0-9]*' | cut -d':' -f2)
     am start -a android.intent.action.VIEW -d "${YOUTUBE_BASE_URL}${youtube_id}" org.mozilla.firefox
     echo "v: $id ($seconds)"
+    sleep 5
+    input tap 500 500
     sleep $((seconds + 5))
     echo "next"
 done
