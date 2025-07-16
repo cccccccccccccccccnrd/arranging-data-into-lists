e236c90e3346988ddba82142181dd7ee3c711bc0
cccccccccccccccccnrd <hello@conradweise.com>
2025-07-16 13:55:56 +0200

    u

--- public/v.sh
+++ public/v.sh
@@ -8,7 +8,7 @@ while true; do
     echo $response
     id=$(echo "$response" | grep -o '"videoId":"[^"]*"' | cut -d'"' -f4)
     seconds=$(echo "$response" | grep -o '"seconds":[0-9]*' | cut -d':' -f2)
-    am start -n org.mozilla.firefox/.App -a android.intent.action.VIEW -d "${YOUTUBE_BASE_URL}${youtube_id}"
+    am start -a android.intent.action.VIEW -d "${YOUTUBE_BASE_URL}${youtube_id}" org.mozilla.firefox
     echo "v: $id ($seconds)"
     sleep $((seconds + 5))
     echo "next"
