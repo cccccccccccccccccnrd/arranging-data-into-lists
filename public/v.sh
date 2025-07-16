c34b6ae659f5b9c5592d6f768f06a569af2e2401
cccccccccccccccccnrd <hello@conradweise.com>
2025-07-16 13:32:10 +0200

    u

--- public/v.sh
+++ public/v.sh
@@ -5,10 +5,11 @@ YOUTUBE_BASE_URL="https://www.youtube.com/watch?v="
 
 while true; do
     response=$(curl -s "$V")
+    echo $response
     id=$(echo "$response" | grep -o '"videoId":"[^"]*"' | cut -d'"' -f4)
-    seconds=$(echo "$response" | grep -o '"seconds":"[^"]*"' | cut -d'"' -f4)
+    seconds=$(echo "$response" | grep -o '"seconds":[0-9]*' | cut -d':' -f2)
     am start -a android.intent.action.VIEW -d "${YOUTUBE_BASE_URL}${id}"
-    echo "v: $id (${seconds})"
+    echo "v: $id ($seconds)"
     sleep $((seconds + 5))
     echo "next"
 done
